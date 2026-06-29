c334a096722a6aed1c372aa37312f0434a14373a
WebActCos <blewis@webact.com>
2026-06-29 17:58:51 -0600

    Load portfolio data part 4

--- about/portfolio/portfolio-data-all.js
+++ about/portfolio/portfolio-data-all.js
@@ -2,3 +2,4 @@ window.webactPortfolioRows=window.webactPortfolioRows||[];
 document.write('<script src="./portfolio-data-part1.js"><\/script>');
 document.write('<script src="./portfolio-data-part2.js"><\/script>');
 document.write('<script src="./portfolio-data-part3.js"><\/script>');
+document.write('<script src="./portfolio-data-part4.js"><\/script>');
