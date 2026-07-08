5bfef5f961ea1f8d87d43f57ffcc6e9853d88b15
WebActCos <blewis@webact.com>
2026-07-07 20:35:58 -0600

    Restore universal header loading

---
+++ assets/js/universal-header-loader.js
@@ -0,0 +1,26 @@
+﻿(function () {
+  function basePath() {
+    return window.location.pathname.indexOf("/webact-redesign/") === 0 ? "/webact-redesign" : "";
+  }
+
+  function loadHeader() {
+    var target = document.getElementById("webact-header");
+    if (!target) return;
+
+    fetch(basePath() + "/includes/header.html", { cache: "no-cache" })
+      .then(function (res) { return res.text(); })
+      .then(function (html) {
+        target.innerHTML = html;
+
+        if (window.WebActPromodoNav && typeof window.WebActPromodoNav.init === "function") {
+          window.WebActPromodoNav.init();
+        }
+      });
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", loadHeader);
+  } else {
+    loadHeader();
+  }
+})();
